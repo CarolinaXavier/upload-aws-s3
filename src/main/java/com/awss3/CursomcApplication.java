fb36d1db8f755012210a53f1f7174a27b12fb68e
Carolina Xavier <carolx54@gmail.com>
2020-04-02 22:17:40 -0300

    first commit

---
+++ src/main/java/com/awss3/CursomcApplication.java
@@ -0,0 +1,24 @@
+package com.awss3;
+import com.awss3.service.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private S3Service s3Service;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursomcApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        s3Service.uploadFile("/home/user/path");
+    }
+
+}
---
+++ src/main/java/com/awss3/exceptions/FileException.java
@@ -0,0 +1,15 @@
+package com.awss3.exceptions;
+
+public class FileException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public FileException(String msg) {
+        super(msg);
+    }
+
+    public FileException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+}
